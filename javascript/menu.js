fe9c0ee04611b3106b3a882fb0e985e5942f0ce7
Graycemuthui <graycemuthui20@gmail.com>
2022-05-20 02:11:48 +0300

    Fix linters

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,16 +1,15 @@
-const navLInks = document.getElementById('navLinks')
-const openBtn = document.getElementById('openBtn')
-const closeBtn = document.getElementById('closeBtn')
-const closeMenu = document.getElementById('navItems')
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const closeMenu = document.getElementById("navItems");
 
 openBtn.onclick = function openBtn() {
-navLinks.style.left = '0';
-}
+  navLinks.style.left = "0";
+};
 
 closeBtn.onclick = function closeBtn() {
-navLinks.style.left = '-200px'
-}
+  navLinks.style.left = "-200px";
+};
 
 closeMenu.onclick = function closeBtn() {
-navLinks.style.left = '-200px'
-}
+  navLinks.style.left = "-200px";
+};
